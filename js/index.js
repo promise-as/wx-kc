f48892c3e73a55e6cee439a82b7fd855e9686840
Hong Baojin <13809735212.163.com>
2020-08-18 18:19:49 +0800

    based finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 charset = 'UTF-8';
 $(function(){
-  
+  function radioHandle(btn, classname){
+    $(btn).click(function(){
+      $(this).addClass(classname).siblings().removeClass(classname);
+    })
+  }
+  radioHandle('.p-choice .c-row span', 'on');
+  // 获取勾选的信息
+  $('.p-choice .submit').click(function(){
+    var text = '';
+    $('.p-choice .on').each(function(){
+      text += $(this).text() + '、'
+    });
+    $('.user-msg').val(text);
+  });
 });
